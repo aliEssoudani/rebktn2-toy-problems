6332ec5101b6a83465c748e895591ee344aaf4f4
aliSnakesou <lkollasfbt@gmail.com>
2020-03-19 09:35:55 +0100

    queue using 2 stack implementation

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -8,20 +8,37 @@ The queue should not have any storage separate from its stacks.
 var Stack = function() {
   var storage = [];
 
-  this.push = function() {};
+  this.push = function(value) {
+    storage.push(value)
+  };
 
-  this.pop = function() {};
+  this.pop = function() {
+    return storage.pop()
+  };
 
-  this.size = function() {};
+  this.size = function() {
+    return storage.length
+  };
 };
 
 var Queue = function() {
   var inbox = new Stack();
   var outbox = new Stack();
 
-  this.enqueue = function() {};
+  this.enqueue = function(value) {
+    inbox.push(value)
+  };
 
-  this.dequeue = function() {};
+  //  we have to reverse the order of element in inbox stack , by saving them in outbox stack using pop nd push ,
+  this.dequeue = function() {
+    while(inbox.size() > 0){
+      // the last in inbox will be the first elemnt at outbox 
+      var temp = inbox.pop();
+      outbox.push(temp)
+    }
+    //  by using pop on outbox now the first value we enqueue is the first returned by dequeue
+    return outbox.pop()
+  };
 
   this.size = function() {};
 };
