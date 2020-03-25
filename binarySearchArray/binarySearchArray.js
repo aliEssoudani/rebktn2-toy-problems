ec0b56caa73f71cfa847a8534cfb8548c1e6cac9
aliSnakesou <lkollasfbt@gmail.com>
2020-03-25 12:33:26 +0100

    forget to push it at 10 :)

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,22 @@
  */
 
 var binarySearch = function (array, target) {
+    var start = 0;
+    var end = array.length - 1 ;
+    while(start <= end){
+        var midInd = Math.floor((start + end)/2);
+        if(array[midInd] === target){
+            return midInd;
+        }
+        else if(array[midInd] < target){
+            start = midInd +1
+        }
+         else{
+            end = midInd -1
+        }
+    }
+    return "target not fount"
+    
 };
+
 
