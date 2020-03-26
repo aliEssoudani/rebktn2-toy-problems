ef2131bcd37e8002dbfe5f2b10aec2bf0745d6dc
aliSnakesou <lkollasfbt@gmail.com>
2020-03-26 10:00:14 +0100

    querystring

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -8,5 +8,16 @@ parseQueryString("http://example.com?a=hello&b=99") // [ [ "a", "hello" ], [ "b"
 parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello world" ] ]
 parseQueryString("http://example.com") // undefined
 */
-
-function parseQueryString(url) {}
+//  it's not working properly for the final result wich contain %
+function parseQueryString(url){
+    //we need just the string after ? mark 
+    var str = url.slice(url.indexOf("?")+1)
+    // console.log(str)
+    var array = str.split("&")
+//     return array;
+    var result = []
+    for(var i  = 0; i < array.length; i++){
+       result.push(array[i].split("=")) 
+    }
+    return result
+} 
