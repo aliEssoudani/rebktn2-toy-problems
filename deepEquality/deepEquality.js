a681f039a6c0198b3eb3117d540bcd939afa069b
aliSnakesou <lkollasfbt@gmail.com>
2020-03-27 09:19:48 +0100

    deepEquality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -5,11 +5,21 @@
   *
   * Examples:
   *
-  * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+  * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3 }}); // true
   * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
   *
   * don't worry about handling cyclical object structures.
   *
   */
 var deepEquals = function(apple, orange) {
+  // we will change our objects to strings than we will just compare the two strings 
+  var a = JSON.stringify(apple);
+  var b = JSON.stringify(orange);
+  // we dont have to worry about the spaces : JSON.stringify({a:1, b:3       }) = JSON.stringify({a:1, b:3}) = "{"a":1,"b":3}"
+ if(a === b){
+   return true
+ }else{
+   return false;
+ }
+  
 };
