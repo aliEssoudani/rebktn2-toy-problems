df7d41be7302cf344cec1c7432d6b2b8a308154f
aliSnakesou <lkollasfbt@gmail.com>
2020-04-03 09:37:01 +0100

    finished

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,22 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var right = 0;
+    var left = 0;
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] === "("){
+            left++
+        }
+        if(str[i] === ")"){
+            right++
+        }
+        
+    }
+    if(right !==left){
+        return false
+    }else{
+        return true
+    }
+
+};
