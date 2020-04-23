3c00f5c5add3817b98273d07b1035df1d1820d74
aliSnakesou <lkollasfbt@gmail.com>
2020-04-23 10:25:56 +0100

    another toy solved

--- characterFrequency/index.js
+++ characterFrequency/index.js
@@ -6,6 +6,21 @@ Write a function that takes as its input a string and returns an array of arrays
 
 function characterFrequency(string) {
   // your code here...
+  var result =[];
+    var array = string.split("");
+    var obj = {};
+    for (let i = 0; i < array.length; i++) {
+      if(obj[array[i]]){
+        obj[array[i]]++
+      }else{
+        obj[array[i]]=1;
+      }
+      
+    }
+    for(var key in obj){
+      result.push([key,obj[key]])
+    }
+    return result
 }
 
 // DO NOT EDIT BELOW THIS LINE
