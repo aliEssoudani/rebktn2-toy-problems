036589bb34530e58197a9fb6d13d55bf452c6367
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-04-24 10:12:59 +0100

    treeMapping

---
+++ treeMapping/test.js
@@ -0,0 +1,108 @@
+const Tree = require('./index');
+const chai = require('chai');
+const should = chai.should();
+
+describe('Tree', () => {
+  it('should exist', () => {
+    should.exist(Tree);
+  });
+  it('should be a function', () => {
+    expect(typeof Tree).toEqual('function');
+  });
+  describe('map()', () => {
+    it('should exist on the Tree prototype', () => {
+      should.exist(Tree.prototype.map);
+    });
+
+    it('should be a function', () => {
+      expect(typeof Tree.prototype.map).toEqual('function');
+    });
+
+    it('should return a Tree instance', () => {
+      var root = new Tree('root');
+      var identity = value => value;
+
+      var result = root.map(identity);
+      should.exist(result);
+      result.should.be.an.instanceOf(Tree);
+    });
+
+    it('should return an identical tree if the mapping function returns the value unaltered', () => {
+      // this "identity" function returns the same value that was passed in
+
+      var identity = value => value;
+      // create a tree with some values
+      // depth: 0
+      var input = new Tree(1);
+      // depth: 1
+      input.addChild(2);
+      input.addChild(3);
+      // depth: 2
+      input.children[0].addChild(4);
+      input.children[0].addChild(5);
+      input.children[1].addChild(6);
+      input.children[1].addChild(8);
+      // depth: 3 (sparse)
+      input.children[0].children[0].addChild(9);
+      input.children[1].children[1].addChild(10);
+
+      var verifyTree = (result, expectation) => {
+        result.should.be.an.instanceOf(Tree); // we expect a tree node
+        result.value.should.equal(expectation.value); // with the same value
+        result.should.not.equal(expectation); // but NOT the same node
+        result.children.should.have.length(expectation.children.length); // with the same number of children
+        for (var i = 0; i < result.children.length; i++) {
+          verifyTree(result.children[i], expectation.children[i]); // and each child is also verified
+        }
+      };
+
+      var result = input.map(identity);
+      // the input and output trees should have identical values
+      verifyTree(result, input);
+    });
+
+    it('should return a tree with doubled values if the function doubles the value', () => {
+      // this function doubles the value that was passed in
+      var double = value => value * 2;
+      // create a tree with some values, and a tree with our *expected* output
+      // depth: 0
+      var input = new Tree(1);
+      var output = new Tree(2);
+      // depth: 1
+      input.addChild(2);
+      input.addChild(3);
+      // expected values
+      output.addChild(4);
+      output.addChild(6);
+      // depth: 2
+      input.children[0].addChild(4);
+      input.children[0].addChild(5);
+      input.children[1].addChild(6);
+      input.children[1].addChild(8);
+      // expected values
+      output.children[0].addChild(8);
+      output.children[0].addChild(10);
+      output.children[1].addChild(12);
+      output.children[1].addChild(16);
+      // depth: 3 (sparse)
+      input.children[0].children[0].addChild(9);
+      input.children[1].children[1].addChild(10);
+      // expected values
+      output.children[0].children[0].addChild(18);
+      output.children[1].children[1].addChild(20);
+
+      var verifyTree = (result, expectation) => {
+        result.should.be.an.instanceOf(Tree); // we expect a tree node
+        result.value.should.equal(expectation.value); // with the same value
+        result.should.not.equal(expectation); // but NOT the same node
+        result.children.should.have.length(expectation.children.length); // with the same number of children
+        for (var i = 0; i < result.children.length; i++) {
+          verifyTree(result.children[i], expectation.children[i]); // and each child is also verified
+        }
+      };
+
+      var result = input.map(double);
+      verifyTree(result, output);
+    });
+  });
+});
