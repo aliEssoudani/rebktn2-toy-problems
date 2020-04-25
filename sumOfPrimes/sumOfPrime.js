cf4dda8bcf40b64ad03409445a838af4dfa8f12b
aliSnakesou <lkollasfbt@gmail.com>
2020-04-25 16:35:19 +0100

    sumPrime

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -5,4 +5,40 @@ Given a positive number, return the sum of all positive primes that are less tha
 
 function sumOfPrimes(n) {
     // your code here...
+   
+    var result = 0;
+   var  primeTester = function (num) {
+        // your code here...
+        var devider = 0;
+        switch(num){
+          case 1 :
+            return false;
+            break;
+          case 2 :
+            return true;
+            break;
+            
+        }
+        for (let i = 2; i <= num; i++) {
+          if(num % i === 0){
+            devider++
+          }
+          if(devider>1){
+            break;
+          }
+          
+        }
+        if(devider > 1){
+          return false
+        }else{
+         return true
+        }
+      }
+    for (let i = 0; i <=n; i++) {
+        if(primeTester(i)){
+            result=result + i
+        }
+        
+    }
+    return result;
 }
