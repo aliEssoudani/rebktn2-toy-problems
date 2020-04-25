f3dbe5cdab34160dba55db588b6a5fe008236095
aliSnakesou <lkollasfbt@gmail.com>
2020-04-25 16:17:33 +0100

    ali

--- characterFrequency/index.js
+++ characterFrequency/index.js
@@ -17,11 +17,47 @@ function characterFrequency(string) {
       }
       
     }
+   console.log(Object.values(obj).sort((a, b) => b-a)) 
+   console.log(obj)
     for(var key in obj){
       result.push([key,obj[key]])
     }
-    return result
+
+    /* let sortFunc = function(a, b) {
+    // if a's value === b's and a's key < b's key return -1
+    // if a's value === b's and a's key > b's key return 1
+    if (b[1] === a[1]) {
+      debugger;
+      if (a[0] < b[0]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } 
+    
+    // if b's value < a's return -1
+    // if b's value > b's return 1
+    return b[1] - a[1];
+  };*/
+    return  result.sort(function(a, b) {
+      //order of frequency descending
+      if (a[1] > b[1]) {
+        return -1;
+      } else if (a[1] < b[1]) {
+        return 1;
+      } 
+      //order of char ascending
+      if (a[0] > b[0]) {
+        return 1;
+      } else if (a[0] < b[0]) {
+        return - 1;
+      }
+      
+      return 0;
+    });
+  
 }
+
 
 // DO NOT EDIT BELOW THIS LINE
 
